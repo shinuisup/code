465a451d171caef4a693fda37ab15e1f39035c8c
julian <shinuisup@gmail.com>
2023-08-21 02:14:36 +0900

    1차완성본

--- project/src/js/main.js
+++ project/src/js/main.js
@@ -1,112 +1,10 @@
 
 (() => {
 
-  // const showCaseElems = document.querySelectorAll('.showcase');
-  // let itemList = showCaseElems[0];
-  // let windowHeight = window.innerHeight;
-  // let ioIndex;
-  // const io = new IntersectionObserver((enteries, observer) => {
-  //   ioIndex = enteries[0].target.dataset.index * 1;
-  // })
-
-  // for (let i = 0; i < showCaseElems.length; i++){
-  //   io.observe(showCaseElems[i]);
-  //   showCaseElems[i].dataset.index = i;
-  // };
-
-  // function acitvate (){
-  //   itemList.classList.add('active');
-  // }
-
-  // function inacitvate (){
-  //   itemList.classList.remove('active');
-  // }
-
-
-  // window.addEventListener('scroll', () => {
-  //   let step;
-  //   let scrollAcition;
-  //   console.log(window.scrollY + window.innerHeight);
-  //   for (let i = 0; i < showCaseElems.length; i++){
-  //     step = showCaseElems[i];
-  //     if (!step) continue;
-  //     scrollAcition = step.getBoundingClientRect().top;
-  //     if (scrollAcition < windowHeight){
-  //       itemList = showCaseElems[step.dataset.index];
-  //       acitvate();
-  //     }else {
-  //       inacitvate();
-  //     }
-  //   };
-  // });
-
-
-  // class CardFlipOnScroll {
-  //   constructor(wrapper, sticky) {
-  //     this.wrapper = wrapper
-  //     this.sticky = sticky
-  //     this.cards = sticky.querySelectorAll('.card')
-  //     this.length = this.cards.length
-  
-  //     this.start = 0
-  //     this.end = 0
-  //     this.step = 0
-  //   }
-  
-  //   init() {
-  //     this.start = this.wrapper.offsetTop - 100
-  //     this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - innerHeight * 1.2
-  //     this.step = (this.end - this.start) / (this.length * 2)
-  //   }
-  
-  //   animate() {
-  //     this.cards.forEach((card, i) => {
-  //       const s = this.start + this.step * i
-  //       const e = s + this.step * (this.length + 1)
-  
-  //       if (scrollY <= s) {
-  //         card.style.transform = `
-  //               perspective(100vw)
-  //               translateX(100vw) 
-  //               rotateY(180deg)
-  //             `
-  //       } else if (scrollY > s && scrollY <= e - this.step) {
-  //         card.style.transform = `
-  //               perspective(100vw)
-  //               translateX(${100 + (scrollY - s) / (e - s) * -100}vw)
-  //               rotateY(180deg)
-  //             `
-  //       } else if (scrollY > e - this.step && scrollY <= e) {
-  //         card.style.transform = `
-  //               perspective(100vw)
-  //               translateX(${100 + (scrollY - s) / (e - s) * -100}vw)
-  //               rotateY(${180 + -(scrollY - (e - this.step)) / this.step * 180}deg)
-  //             `
-  //       } else if (scrollY > e) {
-  //         card.style.transform = `
-  //               perspective(100vw)
-  //               translateX(0vw) 
-  //               rotateY(0deg)
-  //             `
-  //       }
-  //     })
-  //   }
-  // }
-  
-  // const mainContent1 = document.querySelector('.main-content-1')
-  // const sticky = document.querySelector('.sticky')
-  // const cardFlipOnScroll = new CardFlipOnScroll(mainContent1, sticky)
-  // cardFlipOnScroll.init()
-  
-  // window.addEventListener('scroll', () => {
-  //   cardFlipOnScroll.animate()
-  // })
-  
-  // window.addEventListener('resize', () => {
-  //   cardFlipOnScroll.init()
-  // })
 
   const intro = document.querySelector('.showcase-intro');
+  const content = document.querySelector('.event_area');
+  const imgBg = document.querySelector('.showcase-scroll__img-bg');
 
   var animation = function () {
     var items, winH;
@@ -126,9 +24,9 @@
     var _checkPosition = function () {
       for (var i = 0; i < items.length; i++) {
         var posFromTop = items[i].getBoundingClientRect().top;
-        if (winH > posFromTop) {
+        if (winH  * .7 > posFromTop ) {
           items[i].classList.add("active");
-        }else{
+        }else if(winH < posFromTop){
           items[i].classList.remove("active");
         }
       }
@@ -138,13 +36,39 @@
       init: initModule
     }
   }
-    
+
+  const $hour = document.querySelector(".showcase-timer__content-hour");
+  const $minute = document.querySelector(".showcase-timer__content-minute");
+  const $second = document.querySelector(".showcase-timer__content-second");
+
+  function christmasClock() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const christmas = new Date(`${year}-12-25`);
+  
+    const untilChristmas = new Date(christmas - today);
+    // const day = String(Math.floor(untilChristmas / (1000 * 3600 * 24)));
+    const hour = String(
+      Math.floor((untilChristmas / (1000 * 3600)) % 24)).padStart(2, "0");
+    const minute = String(
+      Math.floor((untilChristmas / (1000 * 60)) % 60)).padStart(2, "0");
+    const second = String(Math.floor((untilChristmas / 1000) % 60)).padStart(2,"0");
+    $hour.innerText = `${hour}`;
+    $minute.innerText = `${minute}`;
+    $second.innerText = `${second}`;
+  }
+  
+  const $kv = document.querySelector('.main-kv-video')
+
+
   window.onload = function(){
     animation().init();
     loadSlider();
     setTimeout(() => {
-      intro.style.display = "none";
-    }, "4000");
+      content.classList.add("start");
+      $kv.play();
+     }, "4000");
+    setInterval(christmasClock, 1000);
   }
   window.onbeforeunload = function () {
     window.scrollTo(0, 0);
@@ -158,7 +82,6 @@
       loop: true,
       speed: 3000,
       allowTouchMove: false,
-      disableOnInteraction: true,
       autoplay: {
         delay: 3000,
         disableOnInteraction: true
@@ -173,10 +96,9 @@
       loop: true,
       speed: 800,
       allowTouchMove: false,
-      disableOnInteraction: true,
       autoplay: {
         delay: 3000,
-        disableOnInteraction: true
+        disableOnInteraction: true,
       }
     });
     
@@ -189,10 +111,9 @@
       loop: true,
       speed: 800,
       allowTouchMove: false,
-      disableOnInteraction: true,
       autoplay: {
         delay: 3000,
-        disableOnInteraction: true
+        disableOnInteraction: true,
       }
     });
 
@@ -207,11 +128,9 @@
 
     new Swiper(".showcase-free__slider", {
       freeMode: true,
-      slidesPerView: 'auto',
       loop: true,
-      speed: 4000,
-      slideToClickedSlide: false,
-      allowTouchMove: false,
+      slidesPerView: 'auto',
+      speed: 5000,
       autoplay: {
         delay: 0,
       }
@@ -250,9 +169,62 @@
         type: "progressbar",
       },
     });
-
-
   }
- 
 
 })();
+
+
+class CardFlipOnScroll {
+  constructor(wrapper, sticky) {
+    this.wrapper = wrapper
+    this.sticky = sticky
+    this.cards = sticky.querySelectorAll('.scroll_img')
+    this.length = this.cards.length
+
+    this.start = 0
+    this.end = 0
+    this.step = 0
+  }
+
+  init() {
+    this.start = this.wrapper.offsetTop - 100
+    this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - innerHeight 
+    this.step = (this.end - this.start) / (this.length * 2)
+  }
+
+  animate() {
+    this.cards.forEach((card, i) => {
+      const s = this.start + this.step * i
+      const e = s + this.step * (this.length + 1)
+
+      if (scrollY <= s) {
+        card.style.opacity = '0'
+      } else if (scrollY > s && scrollY <= e - this.step) {
+        card.style.opacity = `
+              ${ (scrollY - s) / (e - s) }
+            `
+      } else if (scrollY > e - this.step && scrollY <= e) {
+        card.style.opacity = `
+              ${(scrollY - s) / (e - s) }
+            `
+      } else if (scrollY > e) {
+        card.style.opacity = `
+             1
+            `
+      }
+    })
+  }
+}
+
+const mainContent1 = document.querySelector('.showcase-scroll')
+const sticky = document.querySelector('.showcase-scroll-sticky')
+const cardFlipOnScroll = new CardFlipOnScroll(mainContent1, sticky)
+cardFlipOnScroll.init()
+
+window.addEventListener('scroll', () => {
+  cardFlipOnScroll.animate()
+})
+
+window.addEventListener('resize', () => {
+  cardFlipOnScroll.init()
+})
